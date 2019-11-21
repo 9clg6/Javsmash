e2737c5b5222d3ca9eb6d128657b98466be098ed
clguyon <clement.guyon@etu.uca.fr>
2019-11-21 09:56:57 +0100

    [FR__] J'ai commencé à faire le menu, tu peux trouver le début de design dans le répertoire Documentation. J'ai un peu modifié la Documentation déjà existante pour rajouter quelques éléments et j'ai amélioré nos Diagrammes. Actuellement j'essaye d'appliquer une image (celle dans /img) comme Background pour la GridPaneFather
    [ENG__]
    I started to make the menu, you can find the beginning of design in the directory Documentation. I modified the existing documentation a little to add some elements and I improved our diagrams.
    Currently I'm trying to apply an image (the one in / img) as Background for the GridPaneFather

---
+++ Code/view/Controller.java
@@ -0,0 +1,19 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+
+import java.net.URL;
+
+public class Controller {
+    @FXML
+    private GridPane GridPaneFather = new GridPane();
+
+    @FXML
+    private Button PlayButton = new Button();
+
+    @FXML private void initialize(){
+
+    }
+
+}
