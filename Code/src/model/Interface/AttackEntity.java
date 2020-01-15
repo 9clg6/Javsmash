18e61ea7f966f4916dc13f7ef707086deae4a834
clguyon <clement.guyon@etu.uca.fr>
2020-01-15 17:46:38 +0100

    Update Interface

--- Code/src/model/Interface/AttackEntity.java
+++ Code/src/model/Interface/AttackEntity.java
@@ -4,6 +4,9 @@ import javafx.scene.layout.Pane;
 import model.hero.Character;
 import model.manager.SkinManager;
 
+/***
+ * Entity for range attack
+ */
 public interface AttackEntity {
     SkinManager skin = null;
     Character character = null;
